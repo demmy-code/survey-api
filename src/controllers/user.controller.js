43c56b2996065649d5967d2673de051f23b97ed9
Demilade <dolaniyan32@gmail.com>
2026-07-02 09:40:18 +0100

    latest changes

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,35 @@
+export const getUserByName = async (req, res) => {
+    try {
+        console.log(req.params)
+        const { name } = req.params;
+        console.log(name)
+        const findUser = await User.findOne({ name });
+        //  const findUser = await User.find({});
+
+        console.log(findUser)
+
+        if (!findUser) {
+            return res.status(404).json({
+                code: 404,
+                message: 'User not found',
+            });
+        }
+
+
+
+        res.status(200).json({
+            code: 200,
+            message: `Welcome ${name}!`,
+            data: {
+                name: findUser.name
+            }
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            code: 500,
+            message: 'Server Error',
+            error: err.message
+        });
+    }
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,41 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { getUserByName } = require('../controllers/user.controller');
 
-router.get('/welcome/:name', async (req, res) => {
-    try {
-        console.log(req.params)
-const { name } = req.params;
-    console.log(name)
-      const findUser = await User.findOne({ name });
-    //  const findUser = await User.find({});
-
-      console.log(findUser)
-
-        if (!findUser) {
-            return res.status(404).json({
-                code: 404,
-                message: 'User not found',
-            });
-        }
-
-       
-
-        res.status(200).json({
-            code: 200,
-            message: `Welcome ${name}!`,
-            data: {
-                name: findUser.name
-            }
-        });
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            code: 500,
-            message: 'Server Error',
-            error: err.message
-        });
-    }
-});
+router.get('/welcome/:name', getUserByName);
 
 module.exports = router;
