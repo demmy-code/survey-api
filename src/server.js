aa795eed8974e1986c3c94eb7e91a377bca67000
Demilade <dolaniyan32@gmail.com>
2026-07-07 16:49:03 +0100

    latest mongoose

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { bufferCommands: false })
   .then(() => console.log('✅ MongoDB Connected!'))
   .catch(err => console.error('❌ Connection Error:', err));
