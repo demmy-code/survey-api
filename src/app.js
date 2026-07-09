7ba53f3812045e86cd406a3d5799f7b5ebc627dd
Demilade <dolaniyan32@gmail.com>
2026-07-09 12:37:05 +0100

    render test 6

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
 
 dotenv.config({quiet: true});
 
+app.use(cors());
 app.use(express.json());
 
 const userRoutes = require('./routes/users');
